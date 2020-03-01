1314aa1ce50f0f3311ba2c30c91d7b3138184e83
Olekcandr Nedochukov <o.nedochukov@gmail.com>
2020-03-01 12:36:08 +0200

    version 0.9.1

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import sys
+
+from PyQt5 import QtWidgets
+from mainWindow import Ui_testing
 
 
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.ui = Ui_testing()
+        self.ui.setupUi(self)
 
+app = QtWidgets.QApplication([])
+application = MyWindow()
+application.show()
+sys.exit(app.exec())
